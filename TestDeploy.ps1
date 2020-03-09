6c054e4ccdde2e219ffab571c37bfaba02f94fd6
danielarumugam <61993313+danielarumugam@users.noreply.github.com>
2020-03-09 14:55:31 -0700

    Update TestDeploy.ps1

--- TestDeploy.ps1
+++ TestDeploy.ps1
@@ -1,5 +1,5 @@
 $connString = "DRIVER={ODBC Driver 13 for SQL Server};PORT=1433;SERVER=scv-sqlserver-dev.database.windows.net;PORT=1443;DATABASE=scv-sqldb-dev;UID=scvetl;PWD=ETLNov2018!;Encrypt=yes;TrustServerCertificate=no"
-
+#test
 $dataTable = Invoke-Sqlcmd -ConnectionString $connString -Query "exec CTRLDB.PROC_TOAPW_OBJ_ADMN_PROC_WRK" -As DataTables
 Write-Output $dataTable.Rows.Count
 foreach ($row in $dataTable.Rows)
