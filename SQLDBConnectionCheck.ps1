b3d02f58060ad3923335b6dc13c243a941fe7e10
danielarumugam <61993313+danielarumugam@users.noreply.github.com>
2020-03-12 18:00:09 -0700

    Update SQLDBConnectionCheck.ps1

--- SQLDBConnectionCheck.ps1
+++ SQLDBConnectionCheck.ps1
@@ -1,4 +1,5 @@
 $connString = "Server=tcp:scv-sqlserver-dev.database.windows.net,1433;Initial Catalog=scv-sqldb-dev;Persist Security Info=False;User ID=scvetl;Password=ETLNov2018!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 #Invoke-Sqlcmd -ConnectionString $connString -Query "select getdate() as dt"
-Write-Output Invoke-Sqlcmd -ConnectionString $connString -Query "Select getdate() dt" 
+#result =  Invoke-Sqlcmd -ConnectionString $connString -Query "Select getdate() dt" 
+Write-Output $result
 
