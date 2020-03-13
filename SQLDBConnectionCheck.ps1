762ac5b5b767f51a6e71fe35a1074fea618dfd07
danielarumugam <61993313+danielarumugam@users.noreply.github.com>
2020-03-12 18:22:25 -0700

    Update SQLDBConnectionCheck.ps1

--- SQLDBConnectionCheck.ps1
+++ SQLDBConnectionCheck.ps1
@@ -1,4 +1,13 @@
-$connString = "Server=tcp:scv-sqlserver-dev.database.windows.net,1433;Initial Catalog=scv-sqldb-dev;Persist Security Info=False;User ID=scvetl;Password=ETLNov2018!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+$credential = Get-AutomationPSCredential -Name "SRVC_PRL"
+$kvName = Get-AutomationVariable -Name 'KV_NM'
+$kvSecretName = Get-AutomationVariable -Name 'SQL_CONNT_STRG_KV_SCRT_NM'
+$tenantId = Get-AutomationVariable -Name 'TNT_ID'
+
+Connect-AzureRMAccount -ServicePrincipal -Credential $Credential -Tenant $tenantId  
+$connString = (Get-AzureKeyVaultSecret -VaultName $kvName -Name $kvSecretName).SecretValueText
+Write-Output $connString
+
+#$connString = "Server=tcp:scv-sqlserver-dev.database.windows.net,1433;Initial Catalog=scv-sqldb-dev;Persist Security Info=False;User ID=scvetl;Password=ETLNov2018!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 #Invoke-Sqlcmd -ConnectionString $connString -Query "select getdate() as dt"
 $result =  Invoke-Sqlcmd -ConnectionString $connString -Query "Select getdate() dt" 
 Write-Output $result
